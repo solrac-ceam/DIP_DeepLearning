70dc6f2b2bec0949e5f9d3b93a3f448cda388f64
Carlos Alfaro <solrac.azathot@gmail.com>
2013-05-09 19:00:12 -0300

    Modify the function: CreateMultibandRndKernel, because the l2norm was wrong; and remove project.h and add dplrgcommon.h

---
+++ dplrgcommon.h
@@ -0,0 +1,18 @@
+#ifndef _DPLRGCOMMON_H_
+#define _DPLRGCOMMON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <float.h>
+#include "mc920.h"
+
+
+#define ACTIVATION_MAX 0
+#define ACTIVATION_NONE -1
+
+
+#endif
